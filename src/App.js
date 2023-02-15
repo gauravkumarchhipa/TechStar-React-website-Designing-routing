f31c8d0ee841dea844670589278487afb32edfb7
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-02-15 22:57:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
+import "./style/App.scss";
+import "./style/Header.scss";
+import "./style/Footer.scss";
+import "./style/Home.scss";
+import "./style/Contact.scss";
+import "./style/mediaquery.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Services from "./Components/Services";
+import Contact from "./Components/Contact";
+import { Footer } from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <Router>
+            <Header/>
+              <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/contact" element={<Contact/>}></Route>
+                <Route path="/services" element={<Services/>}></Route>
+
+              </Routes>
+              <Footer/>
+          </Router>
+        </div>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../style/Footer.scss";
+export const Footer = () => {
+  return (
+    <footer>
+    <div>
+      <h1>TechStar</h1>
+      <p>@all right reserved</p>
+    </div>
+
+    <div>
+      <h5>follow us</h5>
+      <div>
+        <a href="https://github.com/gauravkumarchhipa/" rel="noreferrer" target={"_blank"}>Instagram</a>
+        <a href="https://github.com/gauravkumarchhipa/" rel="noreferrer" target={"_blank"}>Youtube</a>
+        <a href="https://github.com/gauravkumarchhipa/" rel="noreferrer" target={"_blank"}>Github</a>
+        
+      </div>
+    </div>
+    </footer>
+  )
+}
